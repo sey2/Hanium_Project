1c914a4f34797224ad060af967956decdb5c7b97
casio2978 <casio2978@naver.com>
2023-07-24 02:13:39 +0900

    [Feat] Navigation (07.24)

--- app/src/main/java/com/example/hanium/MainActivity.kt
+++ app/src/main/java/com/example/hanium/MainActivity.kt
@@ -2,10 +2,25 @@ package com.example.hanium
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.NavigationUI
+import com.example.hanium.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding : ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        initNavigation()
+
+        setContentView(binding.root)
+    }
+
+    private fun initNavigation() {
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host) as NavHostFragment
+        val mNavController = navHostFragment.navController
+        NavigationUI.setupWithNavController(binding.navBar, mNavController)
     }
 }
---
+++ app/src/main/java/com/example/hanium/UserFragment.kt
@@ -0,0 +1,21 @@
+package com.example.hanium
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+
+class UserFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_user, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+}
