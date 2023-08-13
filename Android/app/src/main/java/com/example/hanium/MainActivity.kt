f5c72e5e770288e01e1473f0e049648a1edd3379
casio2978 <casio2978@naver.com>
2023-08-13 14:13:03 +0900

    [feat] 지피티 연동

--- Android/app/src/main/java/com/example/hanium/MainActivity.kt
+++ Android/app/src/main/java/com/example/hanium/MainActivity.kt
@@ -2,7 +2,6 @@ package com.example.hanium
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import androidx.navigation.findNavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.NavigationUI
 import com.example.hanium.databinding.ActivityMainBinding
