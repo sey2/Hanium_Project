b6851b3e2a09abcc4092744da1d6ca762b1ae9cb
casio2978 <casio2978@naver.com>
2023-09-10 22:11:05 +0900

    [feat] 경락 시세 화면 웹뷰

--- Android/app/src/main/java/com/example/hanium/QuoteFragment.kt
+++ Android/app/src/main/java/com/example/hanium/QuoteFragment.kt
@@ -5,17 +5,34 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.example.hanium.databinding.FragmentQuoteBinding
 
 class QuoteFragment : Fragment() {
 
+    private var _binding: FragmentQuoteBinding? = null
+    private val binding get() = _binding!!
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.fragment_quote, container, false)
+    ): View {
+        _binding = FragmentQuoteBinding.inflate(inflater, container, false)
+        return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        binding.apply {
+            val webViewSetting = wvQuote.settings
+            webViewSetting.javaScriptEnabled = true
+            wvQuote.setInitialScale(90)
+            wvQuote.loadUrl("https://www.susansijang.co.kr/nsis/miw/ko/info/miw3110")
+
+        }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 }
