21685cbc9b6f309679f91b350fc763206a89a424
casio2978 <casio2978@naver.com>
2023-09-17 17:41:58 +0900

    [feat] 화면간 전환 기능

--- Android/app/src/main/java/com/example/hanium/RegisterFragment.kt
+++ Android/app/src/main/java/com/example/hanium/RegisterFragment.kt
@@ -5,32 +5,23 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import androidx.navigation.fragment.findNavController
-import com.example.hanium.databinding.FragmentLoginBinding
-import com.example.hanium.databinding.FragmentUserBinding
+import com.example.hanium.databinding.FragmentRegisterBinding
 
-class UserFragment : Fragment() {
-
-    private var _binding : FragmentUserBinding? = null
+class RegisterFragment : Fragment() {
+    private var _binding : FragmentRegisterBinding? = null
     private val binding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        _binding = FragmentUserBinding.inflate(inflater, container, false)
+        _binding = FragmentRegisterBinding.inflate(inflater, container, false)
 
         return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-
-        binding.apply {
-            ivSetting.setOnClickListener {
-                findNavController().navigate(R.id.action_userFragment_to_userModifyFragment)
-            }
-        }
     }
 
     override fun onDestroyView() {
